fddd55983aa91aae9dc8e56d3b948dc71f956b35
jaseel-am <jaseel.co@gmail.com>
2024-10-17 13:28:31 +0400

    features folder renamed to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'general/inbox', pathMatch: 'full' },
-    { path: 'master', loadChildren: () => import('./features/master/master.module').then(m => m.MasterModule) },
-    { path: 'general', loadChildren: () => import('./features/general/general.module').then(m => m.GeneralModule) },
-    { path: 'transaction', loadChildren: () => import('./features/transaction/transaction.module').then(m => m.TransactionModule) },
-    { path: 'report', loadChildren: () => import('./features/report/report.module').then(m => m.ReportModule) },
+    { path: 'master', loadChildren: () => import('./pages/master/master.module').then(m => m.MasterModule) },
+    { path: 'general', loadChildren: () => import('./pages/general/general.module').then(m => m.GeneralModule) },
+    { path: 'transaction', loadChildren: () => import('./pages/transaction/transaction.module').then(m => m.TransactionModule) },
+    { path: 'report', loadChildren: () => import('./pages/report/report.module').then(m => m.ReportModule) },
     { path: '**', redirectTo: 'report/dashboard' }
 ];
 
